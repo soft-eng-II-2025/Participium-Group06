199543790b623cf60a54684acf2589498a5d93f0
Erestina <erestinavreto1@gmail.com>
2025-11-24 16:13:39 +0100

    Refactor adminController and routes to use officer usernames instead of IDs, and update ReportResponseDTO to include category as a string

--- server/src/routes/tech-leadRoutes.ts
+++ server/src/routes/tech-leadRoutes.ts
@@ -2,26 +2,27 @@ import { Router, Response } from 'express';
 import express from 'express';
 import { requireTechLead } from '../middlewares/authMiddleware';
 import * as adminController from "../controllers/adminController";
+import { User } from '../models/User';
 
 export const router = Router();
 
-router.put('/:officerId/report/:reportId', requireTechLead, async (req, res: Response) => {
-    const officerId = Number(req.params.officerId);
+router.put('/report/:reportId', requireTechLead, async (req, res: Response) => {
     const reportId = Number(req.params.reportId);
 
-    const updatedReport = await adminController.assignTechAgent(reportId, officerId);
+    const username = (req.user as User).username;
+    const updatedReport = await adminController.assignTechAgent(reportId, username);
     res.status(200).json(updatedReport);
 });
 
-router.get('/:id/agents', requireTechLead, async (req, res: Response) => {
-    const techLeadId = Number(req.params.id);
-    const agents = await adminController.getAgentsByTechLeadId(techLeadId);
+router.get('/agents', requireTechLead, async (req, res: Response) => {
+    const username = (req.user as User).username;
+    const agents = await adminController.getAgentsByTechLeadUsername(username);
     res.status(200).json(agents);
 });
 
-router.get('/:id/reports/list', requireTechLead, async (req, res: Response) => {
-    const techLeadId = Number(req.params.id);
-    const reports = await adminController.getTechLeadReports(techLeadId);
+router.get('/reports/list', requireTechLead, async (req, res: Response) => {
+    const username = (req.user as User).username;
+    const reports = await adminController.getTechLeadReports(username);
     res.status(200).json(reports);
 });
 
